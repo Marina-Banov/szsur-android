899536a5067cd6d66ccffdc2dddaffd80f73a53d
mbanov <m.banov7@gmail.com>
2021-09-09 12:29:53 +0200

    info bubbles linked to activities

--- app/src/main/java/hr/uniri/szsur/ui/info/InfoFragment.kt
+++ app/src/main/java/hr/uniri/szsur/ui/info/InfoFragment.kt
@@ -1,18 +1,22 @@
 package hr.uniri.szsur.ui.info
 
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.Gravity
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import androidx.gridlayout.widget.GridLayout
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import hr.uniri.szsur.R
 import hr.uniri.szsur.databinding.FragmentInfoBinding
 import hr.uniri.szsur.databinding.LayoutContactsButtonBinding
+import android.widget.Toast
 
 
 class InfoFragment : Fragment() {
@@ -30,37 +34,51 @@ class InfoFragment : Fragment() {
         viewModel.organization.observe(viewLifecycleOwner, {
             it.contacts.apply {
                 if (web.isNotEmpty()) {
-                    binding.buttonContainer.addView(
-                        createButton(web, R.drawable.ic_globe)
-                    )
+                    val button = createButton(R.drawable.ic_web)
+                    button.setOnClickListener {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(web))
+                        startActivity(intent)
+                    }
                 }
                 if (mail.isNotEmpty()) {
-                    binding.buttonContainer.addView(
-                        createButton(mail, R.drawable.ic_mail)
-                    )
+                    val url = "mailto:${mail}?subject=SZSUR - pitanja"
+                    val button = createButton(R.drawable.ic_mail)
+                    button.setOnClickListener {
+                        val intent = Intent(Intent.ACTION_SENDTO, Uri.parse(url))
+                        startActivity(intent)
+                    }
                 }
                 if (instagram.isNotEmpty()) {
-                    binding.buttonContainer.addView(
-                        createButton(instagram, R.drawable.ic_instagram)
-                    )
-
+                    val button = createButton(R.drawable.ic_instagram)
+                    button.setOnClickListener {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(instagram))
+                        startActivity(intent)
+                    }
                 }
                 if (facebook.isNotEmpty()) {
-                    binding.buttonContainer.addView(
-                        createButton(facebook, R.drawable.ic_facebook)
-                    )
+                    val fbButton = createButton(R.drawable.ic_facebook)
+                    fbButton.setOnClickListener {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/$facebook"))
+                        startActivity(intent)
+                    }
 
-                }
-                if (messenger.isNotEmpty()) {
-                    val url = messenger.replace("m.me/", "fb://messaging/")
-                    binding.buttonContainer.addView(
-                        createButton(url, R.drawable.ic_messenger)
-                    )
+                    val messengerButton = createButton(R.drawable.ic_messenger)
+                    messengerButton.setOnClickListener {
+                        try {
+                            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("fb-messenger://user/$facebook"))
+                                .setPackage("com.facebook.orca")
+                            startActivity(intent)
+                        } catch (ex: ActivityNotFoundException) {
+                            Toast.makeText(context, R.string.no_fb_messenger, Toast.LENGTH_LONG).show()
+                        }
+                    }
                 }
                 if (linkedin.isNotEmpty()) {
-                    binding.buttonContainer.addView(
-                        createButton(linkedin, R.drawable.ic_linkedin)
-                    )
+                    val button = createButton(R.drawable.ic_linkedin)
+                    button.setOnClickListener {
+                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(linkedin))
+                        startActivity(intent)
+                    }
                 }
             }
         })
@@ -68,31 +86,19 @@ class InfoFragment : Fragment() {
         return binding.root
     }
 
-    private fun createButton(target: String, icon: Int): ImageButton {
+    private fun createButton(icon: Int): ImageButton {
         val b: LayoutContactsButtonBinding = DataBindingUtil.inflate(
             LayoutInflater.from(context),
             R.layout.layout_contacts_button,
             null,
             false
         )
+        val params = GridLayout.LayoutParams()
+        params.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, 1f)
+        b.contactsButton.layoutParams = params
         b.contactsButton.setImageResource(icon)
-        b.contactsButton.setOnClickListener {
-            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(target))
-            startActivity(intent)
-        }
+        binding.buttonContainer.addView(b.contactsButton)
+        (b.contactsButton.layoutParams as GridLayout.LayoutParams).setGravity(Gravity.CENTER)
         return b.contactsButton
     }
-/*
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-       super.onViewCreated(view, savedInstanceState)
-
-        var btn_feedback = view.findViewById<Button>(R.id.feedback_btn)
-        btn_feedback.setOnClickListener {
-            var email_info = "mailto:m.banov7@gmail.com?subject=SZSUR -  pitanja"
-            var intent = Intent(Intent.ACTION_SENDTO).apply {
-                setData(Uri.parse(email_info))
-            }
-            startActivity(intent)
-        }
-    }*/
 }
